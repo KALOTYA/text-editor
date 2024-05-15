34aa51e365fac48dd9a8d2187122d9ab2aaeb9fa
Ajay Kaloty <kalotya99@gmail.com>
2024-05-14 20:01:33 -0700

    Adding comments to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,26 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+  //takes content and appends it to the DB
 export const putDb = async (content) => {
-  console.log('Post to the database');
-
+  console.log('POSTdatabase');
+  //connection creation
   const todosDb = await openDB('jate',1);
+  //creating a transaction
   const tx = todosDb.transaction('jate','readwrite');
   const store = tx.objectStore('jate');
+  //adding content to the database
   const request = store.add({jate: content});
 
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('Data saved to the database', result);
 
 }
 
 // TODO: Add logic for a method that gets all the content from the database
+//GET for the content
 export const getDb = async () => {
   console.log('GET all from the database');
-  
+  //connection creation
   const todosDb = await openDB('jate', 1);
+  //create transaction
   const tx = todosDb.transaction('jate','readonly');
   const store = tx.objectStore('jate');
+  //GET for all the content
   const request = store.getAll();
 
   const result = await request;
